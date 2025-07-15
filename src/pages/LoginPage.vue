32704796a0ee0693d2ddea9397f47a7e780a1b7e
Mourice-Adero <aderomourice7@gmail.com>
2025-07-15 11:35:37 +0300

    Added login page styling

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,4 +1,25 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+const username = ref('')
+const password = ref('')
+</script>
 <template>
-  <p>Login</p>
+  <div class="flex justify-center items-center h-lvh">
+    <form class="flex flex-col gap-10 shadow shadow-blue-950 py-20 px-10 w-3/4 md:w-2/5">
+      <input
+        v-model="username"
+        class="p-3 border border-gray-500 rounded-xl w-full"
+        placeholder="Username"
+      />
+      <input
+        v-model="password"
+        class="p-3 border border-gray-500 rounded-xl w-full"
+        type="password"
+        placeholder="Password"
+      />
+      <button class="p-3 border border-gray-500 rounded-xl w-full bg-sky-700" type="submit">
+        Login
+      </button>
+    </form>
+  </div>
 </template>
